bb6c1f26f0c046118acff57c3e8e553618007c80
Marco Schmid <marco.schmid@live.com>
2013-01-27 13:30:00 +0100

    added ajax-functions

--- db/settings.py
+++ db/settings.py
@@ -5,21 +5,37 @@ connection = MongoClient('localhost', 27017)
 
 db = connection.catdoor
 
-settings = db.settings
-settings.remove()
+runtime = db.runtime
+runtime.remove()
 
-
-post = {"balou_id": "123456789123456",
-		"door_state": "unlocked",
-		"balou_state": "indoor",
-		"sunrise": datetime.datetime.utcnow(),
-		"sundawn": datetime.datetime.utcnow(),
+post = {"name": "settings",
+		"balou_id": "123456789123456",
 		"last_update": datetime.datetime.utcnow()			
 		}
+post_id = runtime.insert(post)
+print 'Settings-ID: {}'.format(post_id)
 
-post_id = settings.insert(post)
-print post_id
+post = {"name": "schedule",
+		"sunrise": datetime.datetime.utcnow(),
+		"sundawn": datetime.datetime.utcnow(),
+		"opentime": datetime.datetime.utcnow(),
+		"closetime": datetime.datetime.utcnow(),
+		"last_update": datetime.datetime.utcnow()	
+		}
+post_id = runtime.insert(post)
+print 'Schedule-ID: {}'.format(post_id)
 
-latest_settings = settings.find_one()
+post = {"name": "balou",
+		"balou_state": "indoor",
+		"last_update": datetime.datetime.utcnow()			
+		}
+post_id = runtime.insert(post)
+print 'Balou-ID: {}'.format(post_id)
 
-print latest_settings
+post = {"name": "door",
+		"door_state": "unlocked",
+		"last_update": datetime.datetime.utcnow()			
+		}
+post_id = runtime.insert(post)
+print 'Door-ID: {}'.format(post_id)
+
--- web/main.py
+++ web/main.py
@@ -1,4 +1,10 @@
-from bottle import Bottle, run, static_file
+from bottle import Bottle, run, static_file, request
+from pymongo import MongoClient
+import json
+import datetime
+
+connection = MongoClient('localhost', 27017)
+db = connection.catdoor
 
 app = Bottle()
 
@@ -6,16 +12,42 @@ app = Bottle()
 def hello():
 	return static_file('index.html','static')
 
+@app.get('/schedule')
+def GetSchedule():
+	schedule = db.runtime.find_one({"name": "schedule"})
+	ret = {"opentime": unix_time_millis(schedule['opentime']),
+			"closetime": unix_time_millis(schedule['closetime'])}
+	return ret
+
 @app.get('/balou')
 def GetBalou():
-	return 'Show Balou'
+	balou = db.runtime.find_one({"name": "balou"})
+	ret = {"balou_state": balou['balou_state'],
+			"last_update": unix_time_millis(balou['last_update'])}
+	return ret
 
-@app.get('/catdoor')
-def GetCatdoor():
-	return 'Show Catdoor'
+@app.get('/door')
+def GetDoor():
+	door = db.runtime.find_one({"name": "door"})
+	ret = {"door_state": door['door_state'],
+			"last_update": unix_time_millis(door['last_update'])}
+	return ret
 
-@app.put('/catdoor')
-def PutCatdoor():
-	return 'Edit Catdoor'
+@app.put('/door')
+def PutDoor():
+	door = db.runtime.find_one({"name": "door"})
+	door['door_state'] = request.forms.door_state
+	door['last_update'] = datetime.datetime.utcnow()
+
+	db.runtime.save(door)
+	return 0
+
+def unix_time(dt):
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    delta = dt - epoch
+    return delta.total_seconds()
+
+def unix_time_millis(dt):
+    return unix_time(dt) * 1000.0
 
 run(app, host='0.0.0.0', port=8080, debug=True)
